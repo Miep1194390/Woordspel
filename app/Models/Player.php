4f7c81876943320510f3a21e14ed8109eb41cc35
Miep <mirzaselimovic2005@gmail.com>
2023-06-20 10:03:28 +0200

    Update player

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -1,28 +1,11 @@
 <?php
 
-use App\Models\Player;
+namespace App\Models; // Add or update the correct namespace
 
-// ...
 
-public function saveWord(Request $request, $lobbyId)
+use Illuminate\Http\Request; // Add or update the correct namespace for the Request class
+
+
+class Player
 {
-    $word = $request->input('player_word');
-
-    // Get the authenticated player's ID
-    $playerId = auth()->id();
-
-    // Check if the game already has two words
-    $game = Game::where('game_id', $lobbyId)->count();
-    if ($game >= 2) {
-        return redirect()->back()->with('error', 'The maximum number of words for this game has been reached.');
-    }
-
-    // Save the word to the database with the player ID
-    $game = new Game();
-    $game->game_id = $lobbyId;
-    $game->player_id = $playerId; // Assign the player ID to player_id column
-    $game->word = $word;
-    $game->save();
-
-    return redirect()->back()->with('success', 'Word saved successfully.');
 }
